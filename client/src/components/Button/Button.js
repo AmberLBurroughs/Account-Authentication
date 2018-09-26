17cbfcbf16e787ca58ec8660d77dca38b0460f0c
ambooger <amberlburroughs@gmail.com>
2018-09-26 00:42:21 -0700

    Update Button.js

--- client/src/components/Button/Button.js
+++ client/src/components/Button/Button.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./style.css";
 
 const Button = (props) => (
-    <button className={`btn ${props.float}`} onClick={props.handleBtnClick} required={props.required}> {props.children} </button> 
+    <button className={`btn ${props.float}`} onClick={props.handleBtnClick}> {props.children} </button> 
 );
 
 export default Button;
