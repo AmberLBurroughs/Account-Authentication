352df849fc8a1d4d9e8ff580ed2f83936d5261e6
Amber Burroughs <amberlburroughs@gmail.com>
2019-06-12 13:10:51 -0700

    update user controller,change delete method and add update method

--- Server/controllers/user-controller.js
+++ Server/controllers/user-controller.js
@@ -1,34 +1,34 @@
 const db = require("../models");
 
-const passport = require('passport');
-
 // Defining methods for the UserController
 module.exports = {
-  auth: (req, res) =>(req.isAuthenticated())? res.json(true) : res.json(false),
   find: (req, res) => {
     if(req.isAuthenticated()){
       db.User
-      .findById(req.session.passport.user)
-      .then(dbUser => res.json(dbUser))
-      .catch(err => res.status(422).json(err));
+      .findOne({where:{uuid:req.session.passport.user}})
+        .then(dbUser => res.json(dbUser))
+        .catch(err => res.status(422).json(err));
+    }
+    res.status(401).json(err);  
+  },
+  update: (req, res) => {
+    if(req.isAuthenticated()){
+      db.User
+      .update(req.body,{where:{uuid:req.session.passport.user}})
+        .then(dbUser => res.json(dbUser))
+        .catch(err => res.status(422).json(err));
     }
     res.status(401).json(err);  
   },
   delete: (req, res) => {
     if(req.isAuthenticated()){
       db.User
-      .findById({ uuid: req.session.passport.user })
-      .then(dbUser => dbUser.remove())
-      .then(dbUser => {
-        db.Account
-        .find({where:{ userUUID: req.session.passport.user }})
-        .then(dbAccount => dbAccount.remove())
-        .then(dbUser => res.json(true))
+      .destroy({where:{uuid:req.session.passport.user}})
+        .then(deletedUsed => {
+            res.json(deletedUsed);
+        })
         .catch(err => res.status(422).json(err));
-      })
-      .catch(err => res.status(422).json(err));
     }
     res.status(401).json(err);  
   }
 };
-
