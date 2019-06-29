7737bc235153e84d7c8fcb61518fbb56bd6771ef
Amber Burroughs <amberlburroughs@gmail.com>
2019-06-29 01:46:31 -0700

    add onChange to input

--- client/src/components/Input/Input.js
+++ client/src/components/Input/Input.js
@@ -9,7 +9,17 @@ class Input extends React.Component {
 			backgroundSize: `16px`
 		}
 		return(
-			<input id={this.props.elementID} className="input" type={this.props.inputType} placeholder={this.props.placeholder} style={inputStyles} required={this.props.required} minLength={this.props.size} size={this.props.size}/>
+			<input 
+			id={this.props.elementID} 
+			name={this.props.name} 
+			className="input" 
+			type={this.props.inputType} 
+			placeholder={this.props.placeholder} 
+			style={inputStyles} 
+			required={this.props.required} 
+			minLength={this.props.size} 
+			size={this.props.size}
+			onChange={(e)=>this.props.handleChange(e)}/>
 		);
 	}
 }
